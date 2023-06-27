5a9e739965c4ba338b14b20e34f45d94846f025c
alexiagarcia13 <alexia4rs013@gmail.com>
2023-06-27 18:33:50 -0300

    agregamos contenido

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,10 +1,79 @@
-import React from "react";
-import Title from "./Title"
+import { useEffect } from 'react'
+import '../App.css'
 
-export const ItemListContainer = () =>{
+const MOCK_DATA = [
+    {
+        id: 1,
+        nombre: "Texana Feyre",
+        descripción: "Texana caña media",
+        precio: $6950,
+        img: "../img/venta/texanas/feyre_choco.jpeg"
+    },
+    {
+        id: 2,
+        nombre: "Texas",
+        descripción: "Texana color hielo, media",
+        precio: $6950,
+        img: "../img/venta/texanas/texas_hielo.jpeg"
+    },
+    {
+        id: 3,
+        nombre: "Texas cortas",
+        descripción: "Texana corta",
+        precio: 5500,
+        img: "../img/venta/texanas/texascortas_blancas.jpeg"
+    },
+    {
+        id: 4,
+        nombre: "Texana Serena",
+        descripción: "Texana media color camel",
+        precio: $6950,
+        img: "../img/venta/texanas/tex_serena_camel.jpeg"
+    }
+]
+
+const pedirDatos = () => {
+    return new Promise ((resolve, reject) => {
+        setTimeout(() => {
+            resolve (MOCK_DATA)         
+        }, 5000)
+     })
+} 
+
+const ItemListContainer = () => {
+
+    const [productos, setProcutos] = useState ([])
+    console.log(productos)  
+
+
+    useEffect (() => {
+        pedirDatos()
+            .then((res) => {
+                setProductos(res)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }, [])
+
     return(
-        <Title/>
-    );
+        <div className="list_container">
+            <h2>Las mejores botas del condado</h2>
+            <hr />
+
+            {
+                productos.map((prod) => (
+                    <div>
+                        <h4>{prod.nombre}</h4>
+                        <img src={prod.img} alt={prod.nombre} />
+                        <p>{prod.descripcion}</p>
+                        <p>Precio: ${prod.precio}</p>
+                ))
+           }
+
+        </div>
+    )
+        
 }
 
-export default ItemListContainer;
+export default ItemListContainer
